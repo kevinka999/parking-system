908d3a0acebb752e5393665dd6a26189fca5be3c
unknown <kevinka999@gmail.com>
2020-07-02 13:30:24 -0300

    Correção no request do estacionamento e nova table

--- website/src/pages/Estacionamento/index.js
+++ website/src/pages/Estacionamento/index.js
@@ -1,33 +1,50 @@
-import React, {useState} from 'react';
-import MaterialTable from 'material-table';
+import React, {useState, useEffect} from 'react';
 
-import api from '../../services/api.js';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+import api from '../../services/api';
 
 export default function Estacionamento(){
 
-  const [colunasTabela, setColunasTabela] = useState();
   const [veiculosEstacionados, setVeiculosEstacionados] = useState([]);
 
-  api.get("Estacionamento/Index").then((response) => {
-    inserirColunasTabela();
-    setVeiculosEstacionados(response.data);
-  });
-
-  function inserirColunasTabela(){
-    setColunasTabela([
-      { title: 'Placa Veiculo', field: 'veiculo.placa'},
-      { title: 'Preco Inicial', field: 'preco.valorInicial'},
-      { title: 'Entrada', field: 'dataEntrada'},
-      { title: 'Saida', field: 'dataSaida'},
-      { title: 'Valor', field: 'valorPagar'}
-    ]);
-  }
-  
+  useEffect(() => {
+    async function fetch() {
+      await api.get('Estacionamento/Index').then(({data}) => setVeiculosEstacionados(data))
+    }
+    fetch()
+  }, [])
+ 
   return (
-    <MaterialTable
-      title="Estacionamento"
-      columns={colunasTabela}
-      data={veiculosEstacionados}
-    />
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Placa Veiculo</TableCell>
+            <TableCell>Preco Inicial</TableCell>
+            <TableCell>Data Entrada</TableCell>
+            <TableCell>Data Saida</TableCell>
+            <TableCell>Valor a Pagar</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {veiculosEstacionados.map((veiculo) => (
+            <TableRow key={veiculo.id}>
+              <TableCell>{veiculo.veiculo.placa}</TableCell>
+              <TableCell>{veiculo.preco.valorInicial}</TableCell>
+              <TableCell>{veiculo.dataEntrada}</TableCell>
+              <TableCell>{veiculo.dataSaida}</TableCell>
+              <TableCell>{veiculo.valorPagar}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
